1e2c1f71431e5de94ebc0e43ab9cca06e80c858b
Ksen-N <110607941+Ksen-N@users.noreply.github.com>
2022-08-06 15:36:40 +0300

    Rewrite index.js
    Now no default temperatures and more comfortable for reading code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-let now = new Date();
-let day = now.getDay();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let hour = now.getHours();
-let minute = now.getMinutes();
-let date = document.querySelector("#date");
-if (minute <= 9) {
-  date.innerHTML = days[day] + " " + hour + ":0" + minute;
-} else {
-  date.innerHTML = days[day] + " " + hour + ":" + minute;
+function TimeC(time) {
+  let day = time.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  if (minute <= 9) {
+    return days[day] + " " + hour + ":0" + minute;
+  } else {
+    return days[day] + " " + hour + ":" + minute;
+  }
 }
-let form = document.querySelector(".search-form");
-//Current temperature
+//Current time
+let now = new Date();
+let date = document.querySelector("#date");
+date.innerHTML = TimeC(now);
+//Search for your city
 function dTemp(response) {
   let temperature = Math.round(response.data.main.temp);
   let ntemp = document.querySelector("#d-temp");
@@ -28,23 +31,16 @@ function dTemp(response) {
   let h1 = document.querySelector("#urcity");
   h1.innerHTML = response.data.name;
 }
-//Search for city
-function searchC(event) {
-  event.preventDefault();
+function searchC(city) {
   let apiKey = "ebb3d64cbdc8a91fbd86324a76ac4571";
-  let cityInput = document.querySelector("#city-input");
   let url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-    cityInput.value +
+    city +
     "&appid=" +
     apiKey +
     "&units=metric";
-  let h2 = document.querySelector("#urcity");
-  h2.innerHTML = cityInput.value;
   axios.get(url).then(dTemp);
 }
-form.addEventListener("submit", searchC);
-//Your position
 function CurPosition(position) {
   let apiKey = "ebb3d64cbdc8a91fbd86324a76ac4571";
   let lat = position.coords.latitude;
@@ -58,30 +54,20 @@ function CurPosition(position) {
     apiKey;
   axios.get(url).then(dTemp);
 }
+function SubC(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchC(city);
+}
 function Nav(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(CurPosition);
 }
+
+let form = document.querySelector(".search-form");
+form.addEventListener("submit", SubC);
+
 let mapBut = document.querySelector("#cur-location");
 mapBut.addEventListener("click", Nav);
-/*
-let switchC = document.querySelector("#c-link");
-let switchF = document.querySelector("#f-link");
-let num = document.querySelector("#d-temp");
-function switchCe(event) {
-  let num = document.querySelector("#d-temp");
-  event.preventDefault();
-  let rnum = 59;
-  rnum = Math.ceil(((rnum - 32) * 5) / 9);
-  num.innerHTML = rnum + "°C";
-}
-function switchFe(event) {
-  let num = document.querySelector("#d-temp");
-  event.preventDefault();
-  let rnum = 15;
-  rnum = Math.ceil((rnum * 9) / 5 + 32);
-  num.innerHTML = rnum + "°F";
-}
-switchC.addEventListener("click", switchCe);
-switchF.addEventListener("click", switchFe);
-*/
+
+searchC("Kharkiv");
